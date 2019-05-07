ce895996a91132bc96730c7b94d1731ac2af870a
Ana Riat <t.riat24@gmail.com>
2019-05-07 07:38:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,7 @@
 def (speak_to_grandma)(phrase)
   if phase == "I LOVE YOU, GRANDMA"
     return "I LOVE YOU TOO, PUMPKIN!"
-  
-def speak_to_grandma(speak)
-  if speak == speak.upcase
+elsif
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
