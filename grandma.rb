7bf1b65e21095f9ff3577d4b3851776da75e9016
Ana Riat <t.riat24@gmail.com>
2019-05-07 07:39:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def (speak_to_grandma)(phrase)
   if phase == "I LOVE YOU, GRANDMA"
     return "I LOVE YOU TOO, PUMPKIN!"
-elsif phrase == 
+elsif phrase == phrase.downcase
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
